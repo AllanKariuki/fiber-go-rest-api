c870706eb63fa3e70c9475a15777d5b9e5f1d73c
AllanKariuki <a.kariuki@keshiholdings.com>
2025-12-04 09:57:27 +0300

    feat: user respository

---
+++ repositories/user_repository.go
@@ -0,0 +1,62 @@
+package repositories
+
+import (
+	// "github.com/AllanKariuki/fiber-go-rest-api/config"
+	"github.com/AllanKariuki/fiber-go-rest-api/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(user *models.User) error
+	FindAll() ([]models.User, error)
+	FindByID(id uint) (*models.User, error)
+	FindByEmail(email string) (*models.User, error)
+	Update(user *models.User) error
+	Delete(id uint) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) CreateUser(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindAll() ([]models.User, error) {
+	var users []models.User
+	err := r.db.Find(&users).Error
+	return users, err
+}
+
+func (r *userRepository) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) Update(user *models.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepository) Delete(id uint) error {
+	return r.db.Delete(&models.User{}, id).Error
+}
